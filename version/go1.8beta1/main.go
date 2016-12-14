071a06af6abd38e55f9f8643f0ba90697a68171f
Brad Fitzpatrick <bradfitz@golang.org>
2016-12-14 22:43:03 +0000

    version: add programs to download & run go1.8beta1 and go1.8beta2
    This permits a user with any halfway recent version of Go to run:
    
       $ go get golang.org/x/build/version/go1.8beta1
    
       $ go1.8beta1
       go1.8beta1: not downloaded. Run 'go1.8beta1 download' to install to /home/bradfitz/sdk/go1.8beta1
    
       $ go1.8beta1 download
       Downloaded 0.0% (15225 / 90818333 bytes) ...
       Downloaded 50.1% (45514752 / 90818333 bytes) ...
       Downloaded 100.0% (90818333 / 90818333 bytes).
       Unpacking /home/bradfitz/sdk/go1.8beta1/go1.8beta1.linux-amd64.tar.gz ...
       Unpacked /home/bradfitz/sdk/go1.8beta1/go1.8beta1.linux-amd64.tar.gz.
       Success. You may now run 'go1.8beta1'
    
       $ go1.8beta1 version
       go version go1.8beta1 linux/amd64
    
       $ go1.8beta1 run ~/helloworld.go
       Hello, world from go1.8beta1!
    
    Updates golang/go#18136
    
    Change-Id: I4297df19138facbf90dfab790ab30b942151b096
    Reviewed-on: https://go-review.googlesource.com/34385
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ version/go1.8beta1/main.go
@@ -0,0 +1,24 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.8beta1 command runs the go command from go1.8beta1.
+//
+// To install, run:
+//
+//     $ go get golang.org/x/build/version/go1.8beta1
+//     $ go1.8beta1 download
+//
+// And then use the go1.8beta1 command as if it were your normal go
+// command.
+//
+// See the release notes at https://beta.golang.org/doc/go1.8
+//
+// File bugs at http://golang.org/issues/new
+package main
+
+import "golang.org/x/build/version"
+
+func main() {
+	version.Run("go1.8beta1")
+}
