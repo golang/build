d3e450898693f679c0814d8e0fac980f7fd09eb3
Chris Broadfoot <cbro@golang.org>
2016-12-15 15:38:36 -0800

    version/go1.8beta2: fix typo
    Change-Id: I44bcce4377a5623323e9f03fb9a67bff7b2e90e2
    Reviewed-on: https://go-review.googlesource.com/34504
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- version/go1.8beta2/main.go
+++ version/go1.8beta2/main.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// The go1.8beta2 command runs the go command from go1.8beta1.
+// The go1.8beta2 command runs the go command from go1.8beta2.
 //
 // To install, run:
 //
