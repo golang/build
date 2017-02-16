golang/build

commit b7a944e36647403ebe8feced2d3952b6d409b4c7

all: fix vet errors
It would be nice to run these alongside the tests. There is only one
vet error left in the x/build tree, an unreachable return next to a
TODO.

Change-Id: Ic31d96cdee5bb0f6b9795237ff3b743a4b76e3cf
Reviewed-on: https://go-review.googlesource.com/36898
Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

cmd/coordinator/remote.go
@@ -112,6 +112,7 @@ func handleBuildletCreate(w http.ResponseWriter, r *http.Request) {
 
 	ctx := context.WithValue(context.Background(), buildletTimeoutOpt{}, time.Duration(0))
 	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
 	// Doing a release?
 	if user == "release" || user == "adg" || user == "bradfitz" {

cmd/buildlet/buildlet.go
@@ -1236,7 +1236,7 @@ func configureMacStadium() {
 
 	version, err := exec.Command("sw_vers", "-productVersion").Output()
 	if err != nil {
-		log.Fatal("failed to find sw_vers -productVersion: %v", err)
+		log.Fatalf("failed to find sw_vers -productVersion: %v", err)
 	}
 	majorMinor := regexp.MustCompile(`^(\d+)\.(\d+)`)
 	m := majorMinor.FindStringSubmatch(string(version))

cmd/coordinator/kube.go
@@ -222,7 +222,7 @@ func (p *kubeBuildletPool) GetBuildlet(ctx context.Context, hostType string, lg 
 	bc, err := buildlet.StartPod(ctx, buildletsKubeClient, podName, hostType, buildlet.PodOpts{
 		ProjectID:     buildEnv.ProjectName,
 		ImageRegistry: registryPrefix,
-		Description:   fmt.Sprintf("Go Builder for %s at %s", hostType),
+		Description:   fmt.Sprintf("Go Builder for %s", hostType),
 		DeleteIn:      deleteIn,
 		OnPodCreating: func() {
 			lg.logEventTime("pod_creating")
@@ -412,7 +412,7 @@ func (p *kubeBuildletPool) cleanUpOldPods(ctx context.Context) {
 					}
 					if err == nil && time.Now().Unix() > unixDeadline {
 						stats.DeletedOld++
-						log.Printf("Deleting expired pod %q in zone %q ...", pod.Name)
+						log.Printf("Deleting expired pod %q in zone %q ...", pod.Name, buildEnv.Zone)
 						err = buildletsKubeClient.DeletePod(ctx, pod.Name)
 						if err != nil {
 							log.Printf("problem deleting old pod %q: %v", pod.Name, err)

cmd/gitmirror/gitmirror.go
@@ -1234,7 +1234,7 @@ func readKey() (string, error) {
 	if os.IsNotExist(err) && metadata.OnGCE() {
 		key, err := metadata.ProjectAttributeValue("builder-master-key")
 		if err != nil {
-			return "", fmt.Errorf("-key=%s doesn't exist, and key can't be loaded from GCE metadata: %v", err)
+			return "", fmt.Errorf("-key=%s doesn't exist, and key can't be loaded from GCE metadata: %v", *keyFile, err)
 		}
 		return strings.TrimSpace(key), nil
 	}

cmd/makemac/makemac.go
@@ -69,7 +69,7 @@ func main() {
 
 // State is the state of the world.
 type State struct {
-	mu sync.Mutex `json:"-"`
+	mu sync.Mutex
 
 	Hosts  map[string]int    // IP address -> running Mac VM count (including 0)
 	VMHost map[string]string // "mac_10_8_host2b" => "10.0.0.0"

devapp/dash.go
@@ -206,7 +206,6 @@ func (d *display) short(s interface{}) interface{} {
 	default:
 		return s
 	}
-	return s
 }
 
 // css returns name if cond is true; otherwise it returns the empty string.

kubernetes/client.go
@@ -223,7 +223,7 @@ func (c *Client) DeletePod(ctx context.Context, podName string) error {
 	body, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
-		return fmt.Errorf("failed to read response body: DELETE %q: %v, url, err")
+		return fmt.Errorf("failed to read response body: DELETE %q: %v", url, err)
 	}
 	if res.StatusCode != http.StatusOK {
 		return fmt.Errorf("http error: %d DELETE %q: %q: %v", res.StatusCode, url, string(body), err)

commit bdd3cb5f930dd61c49c3e06330c110d9549c6f65

cmd/coordinator: fix remote buildlet regression
Introduced in CL 36898

Prevented remote Kube buildlets from working.

Change-Id: If401c7833248083893a43c2d594885b4c8de213c
Reviewed-on: https://go-review.googlesource.com/37136
Reviewed-by: Chris Broadfoot <cbro@golang.org>

cmd/coordinator/remote.go
@@ -112,7 +112,9 @@ func handleBuildletCreate(w http.ResponseWriter, r *http.Request) {
 
 	ctx := context.WithValue(context.Background(), buildletTimeoutOpt{}, time.Duration(0))
 	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	// NOTE: don't defer close this cancel. If the context is
+	// closed, the pod is destroyed.
+	// TODO: clean this up.
 
 	// Doing a release?
 	if user == "release" || user == "adg" || user == "bradfitz" {
@@ -153,7 +155,7 @@ func handleBuildletCreate(w http.ResponseWriter, r *http.Request) {
 				log.Print(err)
 				return
 			}
-			log.Printf("created buildlet %v for %v", rb.Name, rb.User)
+			log.Printf("created buildlet %v for %v (%s)", rb.Name, rb.User, bc.String())
 			w.Header().Set("Content-Type", "application/json; charset=utf-8")
 			jenc = append(jenc, '\n')
 			w.Write(jenc)