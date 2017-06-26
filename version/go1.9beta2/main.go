5c6aa2c3b5fb4b9a69e03a075256040e615f131c
Chris Broadfoot <cbro@golang.org>
2017-06-26 13:31:04 -0700

    version: add go1.9beta2
    Change-Id: I7c3d889abfa14e205cb931379c248f1aad8bcd9f
    Reviewed-on: https://go-review.googlesource.com/46718
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ version/go1.9beta2/main.go
@@ -0,0 +1,24 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.9beta2 command runs the go command from go1.9beta2.
+//
+// To install, run:
+//
+//     $ go get golang.org/x/build/version/go1.9beta2
+//     $ go1.9beta2 download
+//
+// And then use the go1.9beta2 command as if it were your normal go
+// command.
+//
+// See the release notes at https://beta.golang.org/doc/go1.9
+//
+// File bugs at https://golang.org/issues/new
+package main
+
+import "golang.org/x/build/version"
+
+func main() {
+	version.Run("go1.9beta2")
+}
