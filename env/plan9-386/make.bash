ef2daf498e7af4d162b1914f27ca0fb60a5bf417
David du Colombier <0intro@gmail.com>
2017-07-24 17:07:50 +0200

    env: remove "exec rio" from lib/profile in Plan 9 image
    In the Plan 9 image, we remove "exec rio" from lib/profile,
    so users without graphical interface support will be able
    to connect to the Plan 9 instance.
    
    Updates golang/go#19956
    
    Change-Id: I64b220d7e8a322b2d8b3a78dfd7773a95cc54f5b
    Reviewed-on: https://go-review.googlesource.com/50890
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- env/plan9-386/make.bash
+++ env/plan9-386/make.bash
@@ -220,6 +220,9 @@ expect -exact "term% "
 send "echo fshalt >>/cfg/helix/cpustart\n"
 
 expect -exact "term% "
+send "echo sed /exec/d \\\$home/lib/profile >\\\$home/lib/profile.new && mv \\\$home/lib/profile.new \\\$home/lib/profile\n"
+
+expect -exact "term% "
 send "auth/wrkey\n"
 
 expect -exact "authid: "
