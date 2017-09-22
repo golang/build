3498ec793c1367cc22d6c79e60caa5f1012db4d9
Andrew Bonventre <andybons@golang.org>
2017-09-22 12:19:37 -0400

    x/build/devapp: filter out changes that contain strings indicating they are not ready
    I would type them in this description but they would cause our
    tooling to interpret them as not ready to be submitted :).
    
    Change-Id: I939942ec1d6644fd24b24f8d81aabad8ae923380
    Reviewed-on: https://go-review.googlesource.com/65395
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- devapp/reviews.go
+++ devapp/reviews.go
@@ -1,3 +1,7 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
@@ -7,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"sort"
+	"strings"
 	"time"
 
 	"golang.org/x/build/maintner"
@@ -24,7 +29,8 @@ type change struct {
 }
 
 type reviewsData struct {
-	Projects []*project
+	Projects     []*project
+	TotalChanges int
 
 	// dirty is set if this data needs to be updated due to a corpus change.
 	dirty bool
@@ -44,13 +50,17 @@ func (s *server) handleReviews(t *template.Template, w http.ResponseWriter, r *h
 	defer s.cMu.RUnlock()
 
 	ownerFilter := r.FormValue("owner")
-	var projects []*project
+	var (
+		projects     []*project
+		totalChanges int
+	)
 	if len(ownerFilter) > 0 {
 		for _, p := range s.data.reviews.Projects {
 			var cs []*change
 			for _, c := range p.Changes {
 				if c.OwnerName() == ownerFilter {
 					cs = append(cs, c)
+					totalChanges++
 				}
 			}
 			if len(cs) > 0 {
@@ -59,10 +69,17 @@ func (s *server) handleReviews(t *template.Template, w http.ResponseWriter, r *h
 		}
 	} else {
 		projects = s.data.reviews.Projects
+		totalChanges = s.data.reviews.TotalChanges
 	}
 
 	var buf bytes.Buffer
-	if err := t.Execute(&buf, struct{ Projects []*project }{projects}); err != nil {
+	if err := t.Execute(&buf, struct {
+		Projects     []*project
+		TotalChanges int
+	}{
+		Projects:     projects,
+		TotalChanges: totalChanges,
+	}); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -77,10 +94,16 @@ func (s *server) updateReviewsData() {
 	s.cMu.Lock()
 	defer s.cMu.Unlock()
 
-	var projects []*project
+	var (
+		projects     []*project
+		totalChanges int
+	)
 	s.corpus.Gerrit().ForeachProjectUnsorted(func(p *maintner.GerritProject) error {
 		proj := &project{GerritProject: p}
 		p.ForeachOpenCL(func(cl *maintner.GerritCL) error {
+			if strings.Contains(cl.Commit.Msg, "DO NOT REVIEW") || strings.Contains(cl.Commit.Msg, "DO NOT SUBMIT") {
+				return nil
+			}
 			c := &change{GerritCL: cl}
 			c.LastUpdate = cl.Commit.CommitTime
 			if len(cl.Messages) > 0 {
@@ -88,6 +111,7 @@ func (s *server) updateReviewsData() {
 			}
 			c.FormattedLastUpdate = c.LastUpdate.Format("2006-01-02")
 			proj.Changes = append(proj.Changes, c)
+			totalChanges++
 			return nil
 		})
 		sort.Slice(proj.Changes, func(i, j int) bool {
@@ -100,6 +124,7 @@ func (s *server) updateReviewsData() {
 		return projects[i].Project() < projects[j].Project()
 	})
 	s.data.reviews.Projects = projects
+	s.data.reviews.TotalChanges = totalChanges
 	s.data.reviews.dirty = false
 }
 
