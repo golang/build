c8d18b7b8d36e880bd515595dbca07968864b5e6
Brad Fitzpatrick <bradfitz@golang.org>
2018-05-14 18:51:11 +0000

    env, buildlet/stage0, xb: build stage0 hermetically, add xb docker support
    We used to build the buildlet/stage0 binary once, upload it to GCS,
    and then future Docker builds of environments would slurp it down back
    off the network (unversioned) and back that copy into its image
    forever.
    
    Now the buildlet/stage0 binary gets hermetically built as part of
    env/*/Dockerfiles and GCS uploads/downloads aren't involved.
    
    While we're at it, move more docker logic out of Makefiles and into
    xb, including things like building dependent images (stage0) and
    verifying the versions of dependent layers (e.g. that "golang:1.10" is
    Go 1.10.2, and not Go 1.10 or Go 1.10.1).
    
    Change-Id: I2a08d951b9fa8143fb3aee3ec7c2cc78d216962e
    Reviewed-on: https://go-review.googlesource.com/113135
    Reviewed-by: Andrew Bonventre <andybons@golang.org>

--- cmd/xb/xb.go
+++ cmd/xb/xb.go
@@ -19,6 +19,7 @@
 package main // import "golang.org/x/build/cmd/xb"
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
@@ -65,6 +66,8 @@ func main() {
 		// gcloud container clusters get-credentials --zone=us-central1-f go
 		// gcloud container clusters get-credentials --zone=us-central1-f buildlets
 		runCmd()
+	case "docker":
+		runDocker()
 	default:
 		log.Fatalf("unknown command %q", cmd)
 	}
@@ -96,6 +99,92 @@ func cmdStrOutput(cmd string, args ...string) string {
 	return ret
 }
 
+var expectedGoLayerVersion = map[string]string{
+	"golang:1.10": "go version go1.10.2 linux/amd64",
+}
+
+func validateGoDockerVersion(layer, want string) {
+	out, _ := exec.Command("docker", "run", "--rm", layer, "go", "version").Output()
+	if strings.TrimSpace(string(out)) == want {
+		return
+	}
+
+	out, err := exec.Command("docker", "pull", layer).CombinedOutput()
+	if err != nil {
+		log.Fatalf("failed to docker pull %s: %v, %s", layer, err, out)
+	}
+
+	out, err = exec.Command("docker", "run", "--rm", layer, "go", "version").CombinedOutput()
+	if strings.TrimSpace(string(out)) == want {
+		return
+	}
+	log.Fatalf("expected docker layer %s to have version %q; got err=%v, output=%s", layer, want, err, out)
+}
+
+func runDocker() {
+	if flag.Arg(1) == "build" {
+		file := "Dockerfile"
+		for i, v := range flag.Args() {
+			if v == "-f" {
+				file = flag.Arg(i + 1)
+			}
+		}
+		layers := fromLayers(file)
+		for _, layer := range layers {
+			if want, ok := expectedGoLayerVersion[layer]; ok {
+				validateGoDockerVersion(layer, want)
+				continue
+			}
+			switch layer {
+			case "golang/buildlet-stage0":
+				log.Printf("building dependent layer %q", layer)
+				buildStage0Container()
+			case "debian:stretch":
+				// TODO: validate version of stretch
+			default:
+				log.Fatalf("unsupported layer %q; don't know how to validate or build", layer)
+			}
+		}
+	}
+
+	for i, v := range flag.Args() {
+		// Replace any occurence of REPO with gcr.io/sybolic-datum-552 or
+		// the staging equivalent. Note that getEnv() is only called if
+		// REPO is already present, so the --prod and --staging flags
+		// aren't required to run "xb docker ..." in general.
+		if strings.Contains(v, "REPO") {
+			flag.Args()[i] = strings.Replace(v, "REPO", "gcr.io/"+getEnv().ProjectName, -1)
+		}
+	}
+
+	runCmd()
+}
+
+// fromLayers returns the layers named in the provided Dockerfile
+// file's FROM statements.
+func fromLayers(file string) (layers []string) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	bs := bufio.NewScanner(f)
+	for bs.Scan() {
+		line := strings.TrimSpace(bs.Text())
+		if !strings.HasPrefix(line, "FROM") {
+			continue
+		}
+		f := strings.Fields(line)
+		if len(f) >= 2 && f[0] == "FROM" {
+			layers = append(layers, f[1])
+		}
+	}
+	if err := bs.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
 func runCmd() {
 	cmd := exec.Command(flag.Arg(0), flag.Args()[1:]...)
 	cmd.Stdout = os.Stdout
@@ -103,6 +192,21 @@ func runCmd() {
 	err := cmd.Run()
 	if err != nil {
 		// TODO: return with exact exit status? when needed.
+		log.Fatal(err)
+	}
+}
+
+func buildStage0Container() {
+	dir, err := exec.Command("go", "list", "-f", "{{.Dir}}", "golang.org/x/build/cmd/buildlet/stage0").Output()
+	if err != nil {
+		log.Fatalf("xb: error running go list to find golang.org/x/build/stage0: %v", err)
+	}
+
+	cmd := exec.Command("make", "docker")
+	cmd.Dir = strings.TrimSpace(string(dir))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
