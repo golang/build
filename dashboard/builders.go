7538b3fd4ca65b988ec3118274a57c792fe88f4d
Brad Fitzpatrick <bradfitz@golang.org>
2018-06-25 18:12:11 +0000

    dashboard: enable trybots for js/wasm
    Fixes golang/go#26015
    
    Change-Id: I25656a8e2918196cf950e29184c783503da9f905
    Reviewed-on: https://go-review.googlesource.com/120775
    Reviewed-by: Andrew Bonventre <andybons@golang.org>

--- dashboard/builders.go
+++ dashboard/builders.go
@@ -1157,9 +1157,11 @@ func init() {
 	addBuilder(BuildConfig{
 		Name:              "js-wasm",
 		HostType:          "host-js-wasm",
-		TryBot:            false,
-		MaxAtOnce:         2,
-		numTryTestHelpers: 0,
+		TryBot:            true,
+		numTryTestHelpers: 4,
+		GoDeps: []string{
+			"3dced519cbabc213df369d9112206986e62687fa", // first passing commit
+		},
 		env: []string{
 			"GOOS=js", "GOARCH=wasm", "GOHOSTOS=linux", "GOHOSTARCH=amd64",
 			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/workdir/go/misc/wasm",
