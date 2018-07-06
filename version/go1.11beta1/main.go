5a0b491d3d31e19cc59780f31d5b51c06e073d84
Akhil Indurti <aindurti@gmail.com>
2018-07-06 00:53:38 -0400

    version/go1.11beta1: change URL for release notes
    Previously, the referenced URL for release notes was
    https://golang.org/doc/go1.11beta1, where there was no document. This
    change points it to https://tip.golang.org/doc/go1.11.
    
    Updates golang/go#26240
    
    Change-Id: Ie42cd9c16bc8ebe9e8f5cc820ff702f577212067
    Reviewed-on: https://go-review.googlesource.com/122409
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- version/go1.11beta1/main.go
+++ version/go1.11beta1/main.go
@@ -12,7 +12,7 @@
 // And then use the go1.11beta1 command as if it were your normal go
 // command.
 //
-// See the release notes at https://golang.org/doc/go1.11beta1
+// See the release notes at https://tip.golang.org/doc/go1.11
 //
 // File bugs at https://golang.org/issues/new
 package main
