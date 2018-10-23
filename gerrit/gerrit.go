453fa3cd1b23f0ead5dd3508281bc1f5433f725a
Giuseppe Valente <gvalente@arista.com>
2018-10-23 20:46:33 +0000

    gerrit: add Client.GetProjectTags method, WebLinkInfo, TagInfo
    Change-Id: Iebe0796ecd65b98b75b73b1f4008fef0177fb9c8
    GitHub-Last-Rev: 5807f37ca6853c0a3d7036fc8fb383b687cc29a3
    GitHub-Pull-Request: golang/build#13
    Reviewed-on: https://go-review.googlesource.com/c/143838
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- gerrit/gerrit.go
+++ gerrit/gerrit.go
@@ -307,6 +307,17 @@ type GitPersonInfo struct {
 	TZOffset int       `json:"tz"`
 }
 
+func (gpi *GitPersonInfo) Equal(v *GitPersonInfo) bool {
+	if gpi == nil {
+		if gpi != v {
+			return false
+		}
+		return true
+	}
+	return gpi.Name == v.Name && gpi.Email == v.Email && gpi.Date.Equal(v.Date) &&
+		gpi.TZOffset == v.TZOffset
+}
+
 type FileInfo struct {
 	Status        string `json:"status"`
 	Binary        bool   `json:"binary"`
@@ -637,7 +648,8 @@ type BranchInfo struct {
 	CanDelete bool   `json:"can_delete"`
 }
 
-// GetProjectBranches returns a project's branches.
+// GetProjectBranches returns the branches for the project name. The branches are stored in a map
+// keyed by reference.
 func (c *Client) GetProjectBranches(ctx context.Context, name string) (map[string]BranchInfo, error) {
 	var res []BranchInfo
 	err := c.do(ctx, &res, "GET", fmt.Sprintf("/projects/%s/branches/", name))
@@ -647,6 +659,71 @@ func (c *Client) GetProjectBranches(ctx context.Context, name string) (map[strin
 	m := map[string]BranchInfo{}
 	for _, bi := range res {
 		m[bi.Ref] = bi
+	}
+	return m, nil
+}
+
+// WebLinkInfo is information about a web link.
+// See https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#web-link-info
+type WebLinkInfo struct {
+	Name     string `json:"name"`
+	URL      string `json:"url"`
+	ImageURL string `json:"image_url"`
+}
+
+func (wli *WebLinkInfo) Equal(v *WebLinkInfo) bool {
+	if wli == nil || v == nil {
+		return false
+	}
+	return wli.Name == v.Name && wli.URL == v.URL && wli.ImageURL == v.ImageURL
+}
+
+// TagInfo is information about a tag.
+// See https://gerrit-review.googlesource.com/Documentation/rest-api-projects.html#tag-info
+type TagInfo struct {
+	Ref       string         `json:"ref"`
+	Revision  string         `json:"revision"`
+	Object    string         `json:"object,omitempty"`
+	Message   string         `json:"message,omitempty"`
+	Tagger    *GitPersonInfo `json:"tagger,omitempty"`
+	Created   TimeStamp      `json:"created,omitempty"`
+	CanDelete bool           `json:"can_delete"`
+	WebLinks  []WebLinkInfo  `json:"web_links,omitempty"`
+}
+
+func (ti *TagInfo) Equal(v *TagInfo) bool {
+	if ti == nil || v == nil {
+		return false
+	}
+	if ti.Ref != v.Ref || ti.Revision != v.Revision || ti.Object != v.Object ||
+		ti.Message != v.Message || !ti.Created.Equal(v.Created) || ti.CanDelete != v.CanDelete {
+		return false
+	}
+	if !ti.Tagger.Equal(v.Tagger) {
+		return false
+	}
+	if len(ti.WebLinks) != len(v.WebLinks) {
+		return false
+	}
+	for i := range ti.WebLinks {
+		if !ti.WebLinks[i].Equal(&v.WebLinks[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// GetProjectTags returns the tags for the project name. The tags are stored in a map keyed by
+// reference.
+func (c *Client) GetProjectTags(ctx context.Context, name string) (map[string]TagInfo, error) {
+	var res []TagInfo
+	err := c.do(ctx, &res, "GET", fmt.Sprintf("/projects/%s/tags/", name))
+	if err != nil {
+		return nil, err
+	}
+	m := map[string]TagInfo{}
+	for _, ti := range res {
+		m[ti.Ref] = ti
 	}
 	return m, nil
 }
@@ -712,6 +789,10 @@ func (c *Client) GetProjects(ctx context.Context, branch string) (map[string]*Pr
 }
 
 type TimeStamp time.Time
+
+func (ts TimeStamp) Equal(v TimeStamp) bool {
+	return ts.Time().Equal(v.Time())
+}
 
 // Gerrit's timestamp layout is like time.RFC3339Nano, but with a space instead of the "T",
 // and without a timezone (it's always in UTC).
