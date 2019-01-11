532581ba916cd72d4191e84308d4f3572cf06fbd
Brad Fitzpatrick <bradfitz@golang.org>
2019-01-11 03:36:07 +0000

    all: add linux build tags for linux-only tools
    Part of getting the x/build repo passing on the builders.
    
    Change-Id: I3f2055cbe91c03ddc0a5152bfdbc0f377f354f47
    Reviewed-on: https://go-review.googlesource.com/c/157441
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- cmd/coordinator/coordinator.go
+++ cmd/coordinator/coordinator.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux
+
 // The coordinator runs the majority of the Go build system.
 //
 // It is responsible for finding build work and executing it,
