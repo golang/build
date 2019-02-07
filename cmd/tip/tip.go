golang/build

commit d0914bad8ebc6bcca085ef2f1411e31a613eff46

cmd/tip: remove the autocert build tag
The build system knows how to use modules now, so no need to hide it.

Change-Id: I9c7a8258d45aa4afc78e562b371a54e2c45a98ee
Reviewed-on: https://go-review.googlesource.com/c/160818
Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

cmd/tip/tip.go
@@ -38,14 +38,6 @@ var (
 	autoCertCacheBucket = flag.String("autocert-bucket", "", "if non-empty, the Google Cloud Storage bucket in which to store the LetsEncrypt cache")
 )
 
-// Hooks that are set non-nil in cert.go if the "autocert" build tag
-// is used.
-var (
-	certInit    func()
-	runHTTPS    func(http.Handler) error
-	wrapHTTPMux func(http.Handler) http.Handler
-)
-
 func main() {
 	flag.Parse()
 

cmd/tip/Dockerfile
@@ -168,7 +168,7 @@ RUN go install cloud.google.com/go/compute/metadata \
 # golang sets GOPATH=/go
 ADD . /go/src/tip
 WORKDIR /go/src/tip
-RUN go install --tags=autocert
+RUN go install
 ENTRYPOINT ["/go/bin/tip"]
 
 # We listen on 8080 (for historical reasons). The service.yaml maps public port 80 to 8080.

cmd/tip/Makefile
@@ -16,7 +16,7 @@ usage:
 
 update-deps:
 	go install golang.org/x/build/cmd/gitlock
-	gitlock --update=Dockerfile --ignore=NONE --tags=autocert golang.org/x/tools/cmd/tip
+	gitlock --update=Dockerfile --ignore=NONE golang.org/x/tools/cmd/tip
 
 docker-image: Dockerfile *.go
 	docker build --force-rm -f Dockerfile --tag=$(IMAGE_PROD):$(VERSION) .

cmd/tip/cert.go
@@ -2,14 +2,6 @@
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
-// +build autocert
-
-// This file contains autocert and cloud.google.com/go/storage
-// dependencies we want to hide by default from the Go build system,
-// which currently doesn't know how to fetch non-golang.org/x/*
-// dependencies. The Dockerfile builds the production binary
-// with this code using --tags=autocert.
-
 package main
 
 import (
@@ -24,15 +16,9 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-func init() {
-	runHTTPS = runHTTPSAutocert
-	certInit = certInitAutocert
-	wrapHTTPMux = wrapHTTPMuxAutocert
-}
-
 var autocertManager *autocert.Manager
 
-func certInitAutocert() {
+func certInit() {
 	var cache autocert.Cache
 	if b := *autoCertCacheBucket; b != "" {
 		sc, err := storage.NewClient(context.Background())
@@ -48,7 +34,7 @@ func certInitAutocert() {
 	}
 }
 
-func runHTTPSAutocert(h http.Handler) error {
+func runHTTPS(h http.Handler) error {
 	s := &http.Server{
 		Addr:    ":https",
 		Handler: h,
@@ -59,6 +45,6 @@ func runHTTPSAutocert(h http.Handler) error {
 	return s.ListenAndServeTLS("", "")
 }
 
-func wrapHTTPMuxAutocert(h http.Handler) http.Handler {
+func wrapHTTPMux(h http.Handler) http.Handler {
 	return autocertManager.HTTPHandler(h)
 }

commit 67eca6f0176f38f009df5c18df5b09b60ad744c1

cmd/tip: fix vet failures causing tests to fail
Follow-up from breakage introduced in CL 160818

I keep forgetting that we have trybots here again.

Change-Id: I64bb5dc1ee033425e92bf9b2f31acb6346bbb9be
Reviewed-on: https://go-review.googlesource.com/c/161617
Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
TryBot-Result: Gobot Gobot <gobot@golang.org>
Reviewed-by: Baokun Lee <nototon@gmail.com>
Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

cmd/tip/tip.go
@@ -52,9 +52,7 @@ func main() {
 		log.Fatalf("Unknown %v value: %q", k, os.Getenv(k))
 	}
 
-	if certInit != nil {
-		certInit()
-	}
+	certInit()
 
 	p := &Proxy{builder: b}
 	go p.run()
@@ -63,22 +61,11 @@ func main() {
 	log.Printf("Starting up tip server for builder %q", os.Getenv(k))
 
 	errc := make(chan error, 1)
-
 	go func() {
-		var httpMux http.Handler = mux
-		if wrapHTTPMux != nil {
-			httpMux = wrapHTTPMux(httpMux)
-		}
-		errc <- http.ListenAndServe(":8080", httpMux)
+		errc <- http.ListenAndServe(":8080", wrapHTTPMux(mux))
 	}()
 	if *autoCertDomain != "" {
-		if runHTTPS == nil {
-			errc <- errors.New("can't use --autocert without building binary with the autocert build tag")
-		} else {
-			go func() {
-				errc <- runHTTPS(mux)
-			}()
-		}
+		go func() { errc <- runHTTPS(mux) }()
 		log.Printf("Listening on port 443 with LetsEncrypt support on domain %q", *autoCertDomain)
 	}
 	if err := <-errc; err != nil {