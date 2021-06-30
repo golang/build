592c098c2ffe100b4b88c47c6f5fe537e7dae0e4
Alexander Rakoczy <alex@golang.org>
2021-06-30 11:45:32 -0400

    env/windows-arm64/macstadium: add image notes and qemu script
    Add barebones instructions for creating a macmini instance that runs a
    Windows ARM64 buildlet in a loop. The instruction templates are from our
    other macstadium builders.
    
    See golang/go#47018 for improvements.
    
    Updates golang/go#47018
    Fixes golang/go#42604
    
    Change-Id: I0bb092aaf99afb12a0e563a69bcb711333dda743
    Reviewed-on: https://go-review.googlesource.com/c/build/+/332492
    Trust: Alexander Rakoczy <alex@golang.org>
    Run-TryBot: Alexander Rakoczy <alex@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Carlos Amedee <carlos@golang.org>

---
+++ env/windows-arm64/macstadium/winloop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while true; do
+  DYLD_LIBRARY_PATH="$HOME/macmini-windows/sysroot-macos-arm64/lib" "$HOME/macmini-windows/sysroot-macos-arm64/bin/qemu-system-aarch64" \
+    -L ./UTM.app/Contents/Resources/qemu \
+    -device ramfb \
+    -cpu max \
+    -smp cpus=8,sockets=1,cores=8,threads=1 \
+    -machine virt,highmem=off \
+    -accel hvf \
+    -accel tcg,tb-size=1536 \
+    -boot menu=on \
+    -m 8192 \
+    -name "Virtual Machine" \
+    -device qemu-xhci,id=usb-bus \
+    -device usb-tablet,bus=usb-bus.0 \
+    -device usb-mouse,bus=usb-bus.0 \
+    -device usb-kbd,bus=usb-bus.0 \
+    -bios "$HOME/macmini-windows/Images/QEMU_EFI.fd" \
+    -device nvme,drive=drive0,serial=drive0,bootindex=0 \
+    -drive "if=none,media=disk,id=drive0,file=$HOME/macmini-windows/Images/win10.qcow2,cache=writethrough" \
+    -device usb-storage,drive=drive2,removable=true,bootindex=1 \
+    -drive "if=none,media=cdrom,id=drive2,file=$HOME/macmini-windows/Images/virtio.iso,cache=writethrough" \
+    -device virtio-net-pci,netdev=net0 \
+    -netdev user,id=net0 \
+    -uuid 41E1CBA2-8837-4224-801B-277336D58A3D \
+    -snapshot \
+    -vnc :3
+  sleep 5
+done
