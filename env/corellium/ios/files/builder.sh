12d1884429d1119f7b7f21686435471f84a536c0
Changkun Ou <hi@changkun.de>
2021-12-03 21:47:13 +0100

    env/corellium: use go install to build buildlet
    The environment bootstrap toolchain should use 1.16 or higher.
    When setting up a new builder environment, the previous go get
    will fail from the current supported toolchain for installing
    buildlet command. Use "go install" instead.
    
    Change-Id: Ie5642e9d91389760f22cb439f47ac4280a4561c4
    Reviewed-on: https://go-review.googlesource.com/c/build/+/369234
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

--- env/corellium/ios/files/builder.sh
+++ env/corellium/ios/files/builder.sh
@@ -6,7 +6,7 @@ export GO_BUILDER_ENV=host-ios-arm64-corellium-ios
 export GOROOT_BOOTSTRAP=$HOME/go-ios-arm64-bootstrap
 export PATH=$HOME/bin:$PATH
 while true; do
-	$GOROOT_BOOTSTRAP/bin/go get golang.org/x/build/cmd/buildlet
+	$GOROOT_BOOTSTRAP/bin/go install golang.org/x/build/cmd/buildlet@latest
 	$HOME/go/bin/buildlet -reverse-type host-ios-arm64-corellium-ios -coordinator farmer.golang.org
 	sleep 1
 done
