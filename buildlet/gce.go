7cdbd32ba0ddd9aa868c5a9a45ad60aaca687312
Michael Pratt <mpratt@google.com>
2022-01-27 16:01:15 -0500

    buildlet: migrate back to COS stable milestone
    The latest COS version (m93) has released an updated kernel containing
    the fix to golang/go#49065, so we no longer need to pin away from it.
    
    For golang/go#49065.
    
    Change-Id: Ie74a432e9229eec5613df7e23ead3f252390ae5f
    Reviewed-on: https://go-review.googlesource.com/c/build/+/381454
    Trust: Michael Pratt <mpratt@google.com>
    Run-TryBot: Michael Pratt <mpratt@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Alex Rakoczy <alex@golang.org>

--- buildlet/gce.go
+++ buildlet/gce.go
@@ -413,7 +413,7 @@ func cosImage(ctx context.Context, svc *compute.Service) (string, error) {
 		return cosCachedImage, nil
 	}
 
-	imList, err := svc.Images.List("cos-cloud").Filter(`(family eq "cos-89-lts")`).Context(ctx).Do()
+	imList, err := svc.Images.List("cos-cloud").Filter(`(family eq "cos-stable")`).Context(ctx).Do()
 	if err != nil {
 		return "", err
 	}
