c3175ea8ba74b2c2f0adacce240f6e1495f49fed
Michael Pratt <mpratt@google.com>
2022-04-01 17:48:15 -0400

    cmd/coordinator: record commit time in RFC3339
    This is consistent with the format used by perfdata.golang.org for
    upload-time and x/perf/cmd/bench for runstamp.
    
    For golang/go#49207.
    
    Change-Id: I0c800629c23eb830803d3017806ca6c9c8907b87
    Reviewed-on: https://go-review.googlesource.com/c/build/+/397655
    Trust: Michael Pratt <mpratt@google.com>
    Run-TryBot: Michael Pratt <mpratt@google.com>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- cmd/coordinator/buildstatus.go
+++ cmd/coordinator/buildstatus.go
@@ -1363,7 +1363,7 @@ func (st *buildStatus) uploadBenchResults(baseline string) (err error) {
 	// Prepend some useful metadata.
 	var b strings.Builder
 	fmt.Fprintf(&b, "experiment-commit: %s\n", st.Rev)
-	fmt.Fprintf(&b, "experiment-commit-time: %s\n", st.RevCommitTime)
+	fmt.Fprintf(&b, "experiment-commit-time: %s\n", st.RevCommitTime.In(time.UTC).Format(time.RFC3339Nano))
 	fmt.Fprintf(&b, "baseline-commit: %s\n", baseline)
 	fmt.Fprintf(&b, "benchmarks-commit: %s\n", st.SubRev)
 	fmt.Fprintf(&b, "post-submit: %t\n", st.trySet == nil)
