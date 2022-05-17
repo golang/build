9ac4f3eb943d993c166f05e05f392e9c255ca032
Than McIntosh <thanm@google.com>
2022-05-17 11:51:08 -0400

    env/windows: correct typo in network selection
    Fix typo: --network=vcp should be --network=vpc in build.bash.
    
    Updates golang/go#35006.
    
    Change-Id: I570fbcbc3f14a43ef2eda0a47f665f7df55ae9ab
    Reviewed-on: https://go-review.googlesource.com/c/build/+/406856
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    Reviewed-by: Alex Rakoczy <alex@golang.org>

--- env/windows/build.bash
+++ env/windows/build.bash
@@ -70,7 +70,7 @@ echo "Creating target instance"
 gcloud compute instances create --machine-type="$MACHINE_TYPE" "$INSTANCE_NAME" \
         --image "$BASE_IMAGE" --image-project "$IMAGE_PROJECT" \
         --project="$PROJECT_ID" --zone="$ZONE" \
-        --network=default-vcp \
+        --network=default-vpc \
         --metadata="buildlet-binary-url=https://storage.googleapis.com/go-builder-data/buildlet.windows-amd64" \
         --metadata-from-file=windows-startup-script-ps1=startup.ps1
 
