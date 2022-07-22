313cec3cb4e996513361456aeccbe35650e92da9
Russ Cox <rsc@golang.org>
2022-07-22 14:52:08 -0400

    cmd/genbootstrap: update for regular use
    A handful of changes to make this possible to use for updating
    the toolchains on all builders.
    
    - Add -rev flag to set revision of toolchain to build.
    - Download specific revision from Gerrit instead of assuming $GOROOT is appropriate.
    - Add revision suffix to tgz output names.
    - Add -upload flag to upload to the right bucket.
    - Add -verbose flag and make the output fairly quiet by default.
    - Allow glob pattern to match known targets.
    
    With these changes, in order to build and upload bootstrap toolchains
    for Go 1.17.13 (the new default) for all toolchains:
    
    	genbootstrap -upload '*'
    
    Or to build a newer one for one system:
    
    	genbootstrap -rev go1.19rc2 -upload linux-loong64
    
    For golang/go#44505.
    
    Change-Id: I09fe6408f91b15018e63af711e71af6576d5e6be
    Reviewed-on: https://go-review.googlesource.com/c/build/+/419082
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Run-TryBot: Russ Cox <rsc@golang.org>

--- cmd/genbootstrap/genbootstrap.go
+++ cmd/genbootstrap/genbootstrap.go
@@ -3,143 +3,300 @@
 // license that can be found in the LICENSE file.
 
 /*
-The genbootstrap command prepares GOROOT_BOOTSTRAP tarballs suitable for
+Genbootstrap prepares GOROOT_BOOTSTRAP tarballs suitable for
 use on builders. It's a wrapper around bootstrap.bash. After
 bootstrap.bash produces the full output, genbootstrap trims it up,
 removing unnecessary and unwanted files.
 
-Usage:  genbootstrap GOOS/GOARCH
+Usage:
+
+	genbootstrap [-upload] [-rev=rev] [-v] GOOS-GOARCH[-suffix]...
+
+The argument list can be a single glob pattern (for example '*'),
+which expands to all known targets matching that pattern.
 */
 package main
 
 import (
+	"bytes"
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
+	"sort"
 	"strings"
 
+	"cloud.google.com/go/storage"
+	"golang.org/x/build/dashboard"
 	"golang.org/x/build/internal/envutil"
 )
 
-var skipBuild = flag.Bool("skip_build", false, "skip bootstrap.bash step; useful during development of cleaning code")
+var skipBuild = flag.String("skip_build", "", "skip bootstrap.bash and reuse output in `dir` instead")
+var upload = flag.Bool("upload", false, "upload outputs to gs://go-builder-data/")
+var verbose = flag.Bool("v", false, "show verbose output")
+var rev = flag.String("rev", "go1.17.13", "build Go at Git revision `rev`")
 
 func usage() {
-	fmt.Fprintln(os.Stderr, "Usage: genbootstrap GOOS/GOARCH")
+	fmt.Fprintln(os.Stderr, "Usage: genbootstrap GOOS-GOARCH[-GO$GOARCH]... (or a glob pattern like '*')")
 	flag.PrintDefaults()
 }
 
 func main() {
 	flag.Usage = usage
 	flag.Parse()
-	if flag.NArg() != 1 {
+	if flag.NArg() < 1 {
 		flag.Usage()
 		os.Exit(2)
 	}
-	f := strings.Split(flag.Arg(0), "/")
-	if len(f) != 2 {
-		flag.Usage()
-		os.Exit(2)
-	}
-	goos, goarch := f[0], f[1]
-	if os.Getenv("GOROOT") == "" {
-		log.Fatalf("GOROOT not set in environment")
-	}
 
-	tgz := filepath.Join(os.Getenv("GOROOT"), "src", "..", "..", "gobootstrap-"+goos+"-"+goarch+".tar.gz")
-	os.Remove(tgz)
-	outDir := filepath.Join(os.Getenv("GOROOT"), "src", "..", "..", "go-"+goos+"-"+goarch+"-bootstrap")
-	if !*skipBuild {
-		os.RemoveAll(outDir)
-		cmd := exec.Command(filepath.Join(os.Getenv("GOROOT"), "src", "bootstrap.bash"))
-		envutil.SetDir(cmd, filepath.Join(os.Getenv("GOROOT"), "src"))
-		envutil.SetEnv(cmd, "GOOS="+goos, "GOARCH="+goarch)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			log.Fatal(err)
-		}
-
-		// bootstrap.bash makes a bzipped tar file too, but it's fat and full of stuff we
-		// dont need it. delete it.
-		os.Remove(outDir + ".tbz")
-	}
-
-	if err := filepath.Walk(outDir, func(path string, fi os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		rel := strings.TrimPrefix(strings.TrimPrefix(path, outDir), "/")
-		base := filepath.Base(path)
-		var pkgrel string // relative to pkg/<goos>_<goarch>/, or empty
-		if strings.HasPrefix(rel, "pkg/") && strings.Count(rel, "/") >= 2 {
-			pkgrel = strings.TrimPrefix(rel, "pkg/")
-			pkgrel = pkgrel[strings.Index(pkgrel, "/")+1:]
-			log.Printf("rel %q => %q", rel, pkgrel)
-		}
-		remove := func() error {
-			if err := os.RemoveAll(path); err != nil {
-				return err
-			}
-			if fi.IsDir() {
-				return filepath.SkipDir
-			}
-			return nil
-		}
-		switch pkgrel {
-		case "cmd":
-			return remove()
-		}
-		switch rel {
-		case "api",
-			"bin/gofmt",
-			"doc",
-			"misc/android",
-			"misc/cgo",
-			"misc/chrome",
-			"misc/swig",
-			"test":
-			return remove()
-		}
-		if base == "testdata" {
-			return remove()
-		}
-		if strings.HasPrefix(rel, "pkg/tool/") {
-			switch base {
-			case "addr2line", "api", "cgo", "cover",
-				"dist", "doc", "fix", "nm",
-				"objdump", "pack", "pprof",
-				"trace", "vet", "yacc":
-				return remove()
+	list := flag.Args()
+	if len(list) == 1 && strings.ContainsAny(list[0], "*?[]") {
+		pattern := list[0]
+		list = nil
+		for _, name := range allPairs() {
+			if ok, err := path.Match(pattern, name); ok {
+				list = append(list, name)
+			} else if err != nil {
+				log.Fatalf("invalid match: %v", err)
 			}
 		}
-		if fi.IsDir() {
-			return nil
+		if len(list) == 0 {
+			log.Fatalf("no matches for %q", pattern)
 		}
-		if isEditorJunkFile(path) {
-			return remove()
+		log.Printf("expanded %s: %v", pattern, list)
+	}
+
+	for _, pair := range list {
+		f := strings.Split(pair, "-")
+		if len(f) != 2 && len(f) != 3 {
+			log.Fatalf("invalid target: %q", pair)
 		}
-		if !fi.Mode().IsRegular() {
-			return remove()
-		}
-		if strings.HasSuffix(path, "_test.go") {
-			return remove()
-		}
-		log.Printf("keeping: %s\n", rel)
-		return nil
-	}); err != nil {
+	}
+
+	dir, err := os.MkdirTemp("", "genbootstrap-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	goroot := filepath.Join(dir, "goroot")
+	if err := os.MkdirAll(goroot, 0777); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Running: tar zcf %s .", tgz)
-	cmd := exec.Command("tar", "zcf", tgz, ".")
-	envutil.SetDir(cmd, outDir)
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("tar zf failed: %v", err)
+	log.Printf("Bootstrapping in %s at revision %s\n", dir, *rev)
+
+	resp, err := http.Get("https://go.googlesource.com/go/+archive/" + *rev + ".tar.gz")
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Printf("Done. Output is %s", tgz)
+	if resp.StatusCode != 200 {
+		body, _ := io.ReadAll(io.LimitReader(resp.Body, 64<<10))
+		log.Fatalf("fetching %s: %v\n%s", *rev, resp.Status, body)
+	}
+
+	cmd := exec.Command("tar", "-C", goroot, "-xzf", "-")
+	cmd.Stdin = resp.Body
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("tar: %v\n%s", err, out)
+	}
+
+	// Work around GO_LDSO bug by removing implicit setting from make.bash.
+	// See go.dev/issue/54196 and go.dev/issue/54197.
+	// Even if those are fixed, the old toolchains we are using for bootstrap won't get the fix.
+	makebash := filepath.Join(goroot, "src/make.bash")
+	data, err := os.ReadFile(makebash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data = bytes.ReplaceAll(data, []byte("GO_LDSO"), []byte("GO_LDSO_BUG"))
+	if err := os.WriteFile(makebash, data, 0666); err != nil {
+		log.Fatal(err)
+	}
+
+	var storageClient *storage.Client
+	if *upload {
+		ctx := context.Background()
+		storageClient, err = storage.NewClient(ctx)
+		if err != nil {
+			log.Fatalf("storage.NewClient: %v", err)
+		}
+	}
+
+	gorootSrc := filepath.Join(goroot, "src")
+List:
+	for _, pair := range list {
+		f := strings.Split(pair, "-")
+		goos, goarch, gosuffix := f[0], f[1], ""
+		if len(f) == 3 {
+			gosuffix = "-" + f[2]
+		}
+
+		log.Printf("# %s-%s%s\n", goos, goarch, gosuffix)
+
+		tgz := filepath.Join(dir, "gobootstrap-"+goos+"-"+goarch+gosuffix+"-"+*rev+".tar.gz")
+		os.Remove(tgz)
+		outDir := filepath.Join(dir, "go-"+goos+"-"+goarch+"-bootstrap")
+		if *skipBuild != "" {
+			outDir = *skipBuild
+		} else {
+			os.RemoveAll(outDir)
+			cmd := exec.Command(filepath.Join(gorootSrc, "bootstrap.bash"))
+			envutil.SetDir(cmd, gorootSrc)
+			envutil.SetEnv(cmd,
+				"GOROOT="+goroot,
+				"CGO_ENABLED=0",
+				"GOOS="+goos,
+				"GOARCH="+goarch,
+				"GOROOT_BOOTSTRAP="+os.Getenv("GOROOT_BOOTSTRAP"),
+			)
+			if gosuffix != "" {
+				envutil.SetEnv(cmd, "GO"+strings.ToUpper(goarch)+"="+gosuffix[len("-"):])
+			}
+			if *verbose {
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				if err := cmd.Run(); err != nil {
+					log.Print(err)
+					continue List
+				}
+			} else {
+				if out, err := cmd.CombinedOutput(); err != nil {
+					os.Stdout.Write(out)
+					log.Print(err)
+					continue List
+				}
+			}
+
+			// bootstrap.bash makes a bzipped tar file too,
+			// but it's fat and full of stuff we don't need. Delete it.
+			os.Remove(outDir + ".tbz")
+		}
+
+		if err := filepath.Walk(outDir, func(path string, fi os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			rel := strings.TrimPrefix(strings.TrimPrefix(path, outDir), "/")
+			base := filepath.Base(path)
+			var pkgrel string // relative to pkg/<goos>_<goarch>/, or empty
+			if strings.HasPrefix(rel, "pkg/") && strings.Count(rel, "/") >= 2 {
+				pkgrel = strings.TrimPrefix(rel, "pkg/")
+				pkgrel = pkgrel[strings.Index(pkgrel, "/")+1:]
+				if *verbose {
+					log.Printf("rel %q => %q", rel, pkgrel)
+				}
+			}
+			remove := func() error {
+				if err := os.RemoveAll(path); err != nil {
+					return err
+				}
+				if fi.IsDir() {
+					return filepath.SkipDir
+				}
+				return nil
+			}
+			switch pkgrel {
+			case "cmd":
+				return remove()
+			}
+			switch rel {
+			case "api",
+				"bin/gofmt",
+				"doc",
+				"misc/android",
+				"misc/cgo",
+				"misc/chrome",
+				"misc/swig",
+				"test":
+				return remove()
+			}
+			if base == "testdata" {
+				return remove()
+			}
+			if strings.HasPrefix(rel, "pkg/tool/") {
+				switch base {
+				case "addr2line", "api", "cgo", "cover",
+					"dist", "doc", "fix", "nm",
+					"objdump", "pack", "pprof",
+					"trace", "vet", "yacc":
+					return remove()
+				}
+			}
+			if fi.IsDir() {
+				return nil
+			}
+			if isEditorJunkFile(path) {
+				return remove()
+			}
+			if !fi.Mode().IsRegular() {
+				return remove()
+			}
+			if strings.HasSuffix(path, "_test.go") {
+				return remove()
+			}
+			if *verbose {
+				log.Printf("keeping: %s\n", rel)
+			}
+			return nil
+		}); err != nil {
+			log.Print(err)
+			continue List
+		}
+
+		cmd := exec.Command("tar", "zcf", tgz, ".")
+		envutil.SetDir(cmd, outDir)
+		if *verbose {
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				log.Print(err)
+				continue List
+			}
+		} else {
+			if out, err := cmd.CombinedOutput(); err != nil {
+				os.Stdout.Write(out)
+				log.Print(err)
+				continue List
+			}
+		}
+
+		log.Printf("Built %s", tgz)
+		if *upload {
+			project := "symbolic-datum-552"
+			bucket := "go-builder-data"
+			object := filepath.Base(tgz)
+			w := storageClient.Bucket(bucket).Object(object).NewWriter(context.Background())
+			// If you don't give the owners access, the web UI seems to
+			// have a bug and doesn't have access to see that it's public, so
+			// won't render the "Shared Publicly" link. So we do that, even
+			// though it's dumb and unnecessary otherwise:
+			w.ACL = append(w.ACL, storage.ACLRule{Entity: storage.ACLEntity("project-owners-" + project), Role: storage.RoleOwner})
+			w.ACL = append(w.ACL, storage.ACLRule{Entity: storage.AllUsers, Role: storage.RoleReader})
+			f, err := os.Open(tgz)
+			if err != nil {
+				log.Print(err)
+				continue List
+			}
+			w.ContentType = "application/octet-stream"
+			_, err1 := io.Copy(w, f)
+			f.Close()
+			err = w.Close()
+			if err == nil {
+				err = err1
+			}
+			if err != nil {
+				log.Printf("Failed to upload %s: %v", tgz, err)
+				continue List
+			}
+			log.Printf("Uploaded gs://%s/%s", bucket, object)
+		}
+	}
 }
 
 func isEditorJunkFile(path string) bool {
@@ -151,4 +308,34 @@ func isEditorJunkFile(path string) bool {
 		return true
 	}
 	return false
+}
+
+// allPairs returns a list of all known builder GOOS/GOARCH pairs.
+func allPairs() []string {
+	have := make(map[string]bool)
+	var list []string
+	add := func(name string) {
+		if !have[name] {
+			have[name] = true
+			list = append(list, name)
+		}
+	}
+
+	for _, b := range dashboard.Builders {
+		f := strings.Split(b.Name, "-")
+		switch f[0] {
+		case "android", "ios", "js", "misc":
+			// skip
+			continue
+		}
+		name := f[0] + "-" + f[1]
+		if f[1] == "arm" {
+			add(name + "-5")
+			add(name + "-7")
+			continue
+		}
+		add(name)
+	}
+	sort.Strings(list)
+	return list
 }
