f08b7e722ab9307890362cea2aed8b19e1c5cde6
Paul E. Murphy <murp@ibm.com>
2022-08-30 17:12:39 -0500

    cmd/buildlet/stage0: define env for host-linux-ppc64-sid
    This should be identical to host-linux-ppc64-osu.
    
    Change-Id: I673e64469ee3dba92cb772c739776a558fb4ef49
    Reviewed-on: https://go-review.googlesource.com/c/build/+/426914
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    Reviewed-by: David Chase <drchase@google.com>
    Run-TryBot: Paul Murphy <murp@ibm.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- cmd/buildlet/stage0/stage0.go
+++ cmd/buildlet/stage0/stage0.go
@@ -171,7 +171,7 @@ Download:
 		cmd.Args = append(cmd.Args, reverseHostTypeArgs(buildEnv)...)
 	case "host-linux-ppc64le-osu": // power8
 		cmd.Args = append(cmd.Args, reverseHostTypeArgs(buildEnv)...)
-	case "host-linux-ppc64-osu":
+	case "host-linux-ppc64-osu", "host-linux-ppc64-sid":
 		cmd.Args = append(cmd.Args, reverseHostTypeArgs(buildEnv)...)
 	case "host-linux-amd64-wsl", "host-linux-riscv64-unmatched":
 		cmd.Args = append(cmd.Args, reverseHostTypeArgs(buildEnv)...)
