c31a5bb4f26c53085efeb53ed570247eaad5cb2e
Heschi Kreinick <heschi@google.com>
2023-06-29 11:13:59 -0400

    cmd/buildlet/stage0: add LUCI environment setup
    Improve run-worker to successfully start up LUCI. It depends on sudo, so
    add it to all of our images.
    
    The resulting image is up and running as a LUCI worker, and debugnewvm
    on it also works fine.
    
    Change-Id: Ibcac4acf5f9849e5847ae926e333002108c08a0d
    Reviewed-on: https://go-review.googlesource.com/c/build/+/508866
    Run-TryBot: Heschi Kreinick <heschi@google.com>
    Auto-Submit: Heschi Kreinick <heschi@google.com>
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- cmd/buildlet/stage0/run-worker.sh
+++ cmd/buildlet/stage0/run-worker.sh
@@ -4,10 +4,27 @@
 # license that can be found in the LICENSE file.
 
 set -o pipefail
+
+trap "exit 10" SIGUSR1
+
 project=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/project/project-id)
 
-worker=stage0
-if [[ $project =~ "luci" ]]; then
-    worker=bootstrapswarm
+if [[ ! $project =~ "luci" ]]; then
+    exec $(dirname $0)/stage0
 fi
-exec $(dirname $0)/$worker
+
+if ! id swarming >& /dev/null; then
+    useradd -m swarming
+fi
+
+echo "%swarming ALL = NOPASSWD: /sbin/shutdown" > /etc/sudoers.d/swarming
+echo -e '#!/bin/bash\nkill -SIGUSR1 1' > /sbin/shutdown
+chmod 0755 /sbin/shutdown
+
+SWARM_DIR=/b/swarming
+mkdir -p $SWARM_DIR
+chown swarming:swarming $SWARM_DIR
+
+su -c "$(dirname $0)/bootstrapswarm" swarming &
+wait %1
+exit $?
