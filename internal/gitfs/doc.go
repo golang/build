40c0547c51b7978c3e98c72157161cf6d7b7b4f2
Dmitri Shuralyov <dmitshur@golang.org>
2024-06-13 14:08:10 -0400

    internal/gitfs: add a generated copy of x/website/internal/gitfs
    It's generated rather than copied manually, so that it is clear its
    canonical source still lives at golang.org/x/website/internal/gitfs.
    
    For golang/go#68873.
    
    Change-Id: I2ec03384666d5c230e59c36db118e7f3969a8e11
    Reviewed-on: https://go-review.googlesource.com/c/build/+/617777
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Auto-Submit: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ internal/gitfs/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package gitfs presents a file tree downloaded from a remote Git repo as an in-memory fs.FS.
+package gitfs
