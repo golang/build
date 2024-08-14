78356339bbe926d4e2cd4f4c792d64947f0da251
Michael Anthony Knyszek <mknyszek@google.com>
2024-08-14 18:43:11 +0000

    cmd/gomote: apply swarmingBuilders timeout only to the RPC
    Currently the timeout also applies to the interactive login, which is a
    pain. It's also not obvious that someone should just run a different
    command.
    
    Fixes golang/go#68875.
    
    Change-Id: I1f4d10dc8b09df0d928a4fd3429a4eb2b5ccf184
    Reviewed-on: https://go-review.googlesource.com/c/build/+/605317
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- cmd/gomote/create.go
+++ cmd/gomote/create.go
@@ -79,10 +79,11 @@ func builders() (bt []builderType) {
 }
 
 func swarmingBuilders() ([]string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
+	ctx := context.Background()
 	client := gomoteServerClient(ctx)
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
 	resp, err := client.ListSwarmingBuilders(ctx, &protos.ListSwarmingBuildersRequest{})
 	if err != nil {
 		return nil, fmt.Errorf("unable to retrieve swarming builders: %s", err)
