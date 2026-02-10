95efed9df7dbd8bc725de1852bf1210ae43e3c43
Dmitri Shuralyov <dmitshur@golang.org>
2026-02-10 11:35:01 -0500

    cmd/devapp: update source code link after move
    Add the "cmd" path component was missed in CL 737061.
    Point to the source code viewer at https://go.dev/cs.
    While here, apply the same change to the owners page.
    
    Change-Id: Ie7edd0c36068240670f7444bf7cd12f312043c4f
    Reviewed-on: https://go-review.googlesource.com/c/build/+/743920
    Auto-Submit: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>

--- devapp/owners/owners.go
+++ devapp/owners/owners.go
@@ -349,8 +349,7 @@ body {
 <header class="header">
 	<p>Reviews are automatically assigned to primary owners.</p>
 	<p>Alter these entries at
-	<a href="https://go.googlesource.com/build/+/master/devapp/owners"
-		target="_blank" rel="noopener">golang.org/x/build/devapp/owners</a>.</p>
+	<a href="https://go.dev/cs/x/build/+/HEAD:devapp/owners/">golang.org/x/build/devapp/owners</a>.</p>
 </header>
 <main>
 <div class="table-header">
