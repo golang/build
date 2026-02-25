1b107cb27f55f55bae7871135ed1ba8cb63ab96c
gurusai-voleti <gvoleti@google.com>
2026-02-25 08:27:14 +0000

    chore: Migrate gsutil usage to gcloud storage

--- env/windows-arm64/azure/setupAndRunAllDotBat.sh
+++ env/windows-arm64/azure/setupAndRunAllDotBat.sh
@@ -51,8 +51,8 @@ function run_command_on_vm() {
 function copy_from_go_builder_data() {
   local FILE="$1"
   local TGT="$2"
-  echo "... executing: gsutil cp gs://go-builder-data/${FILE} $TGT"
-  gsutil cp gs://go-builder-data/${FILE} $TGT
+  echo "... executing: gcloud storage cp gs://go-builder-data/${FILE} $TGT"
+  gcloud storage cp gs://go-builder-data/${FILE} $TGT
   if [ $? != 0 ]; then
     echo "error: copy from gs://go-builder-data/${FILE} failed, aborting"
     exit 1
